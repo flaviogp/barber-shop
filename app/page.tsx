8f8ba86b9516dfd4534cbb230c6b2f8f8c0e439a
flaviogp <flavio.gpinheiro@hotmail.com>
2024-09-20 09:46:43 -0300

    feat: add booking item

--- app/page.tsx
+++ app/page.tsx
@@ -4,29 +4,57 @@ import { Input } from "./_components/ui/input"
 import { Button } from "./_components/ui/button"
 import { SearchIcon } from "lucide-react"
 import Image from "next/image"
+import { Card, CardContent } from "./_components/ui/card"
+import { Badge } from "./_components/ui/badge"
+import { Avatar, AvatarImage } from "./_components/ui/avatar"
 
 const page = () => {
   return (
     <div>
       <Header />
       <div className="p-5">
+        {/* Texto */}
         <h2 className="text-xl font-bold">Olá, Flavio</h2>
         <p>Quinta feira, 19 de setembro</p>
 
+        {/* Busca */}
         <div className="mt-6 flex items-center gap-2">
           <Input placeholder="Faça a sua busca" />
           <Button>
             <SearchIcon />
           </Button>
         </div>
-
-        <div className="relative mt-6 h-[150px] w-full"></div>
-        <Image
-          src="/banner-01.png"
-          alt="Agende com os melhores"
-          fill
-          className="rounded-xl object-contain"
-        />
+        {/* Imagem */}
+        <div className="relative mt-6 h-[150px] w-full">
+          <Image
+            src="/banner-01.png"
+            alt="Agende com os melhores"
+            fill
+            className="rounded-xl object-contain"
+          />
+        </div>
+        {/* Agendamento */}
+        <Card className="mt-6">
+          <CardContent className="flex justify-between p-0">
+            {/* Left */}
+            <div className="flex flex-col gap-2 py-5 pl-5">
+              <Badge className="w-fit">Confirmado</Badge>
+              <h3 className="font-semibold">Corte de Cabelo</h3>
+              <div className="flex items-center gap-2">
+                <Avatar className="h-6 w-6">
+                  <AvatarImage src="https://conteudo.imguol.com.br/c/entretenimento/80/2017/04/25/a-atriz-zoe-saldana-como-neytiri-em-avatar-1493136439818_v2_3x4.jpg" />
+                </Avatar>
+                <p className="text-sm">Barbearia</p>
+              </div>
+            </div>
+            {/* Right */}
+            <div className="flex flex-col items-center justify-center border-l-2 border-solid px-5">
+              <p className="text-sm">Agosto</p>
+              <p className="text-2xl">05</p>
+              <p className="text-sm">20:00</p>
+            </div>
+          </CardContent>
+        </Card>
       </div>
     </div>
   )
