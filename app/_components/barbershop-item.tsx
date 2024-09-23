f19353ac03502ee0266a1716e0fdab94a4d055df
flaviogp <flavio.gpinheiro@hotmail.com>
2024-09-23 11:07:46 -0300

    feat: add BarberShopItem component

---
+++ app/_components/barbershop-item.tsx
@@ -0,0 +1,46 @@
+import { BarberShop } from "@prisma/client"
+import React from "react"
+import { Card, CardContent } from "./ui/card"
+import Image from "next/image"
+import { Button } from "./ui/button"
+import { Badge } from "./ui/badge"
+import { StarIcon } from "lucide-react"
+
+interface BarberShopItemProps {
+  barberShop: BarberShop
+}
+
+const BarbershopItem = ({ barberShop }: BarberShopItemProps) => {
+  return (
+    <Card className="min-w-[167px] rounded-2xl">
+      <CardContent className="p-0 px-1 pt-1">
+        {/* Image  */}
+        <div className="relative h-[159px] w-full">
+          <Image
+            fill
+            alt={barberShop.name}
+            className="rounded-2xl object-cover"
+            src={barberShop.imageUrl}
+          />
+          <Badge
+            className="absolute left-2 top-2 space-x-1"
+            variant="secondary"
+          >
+            <StarIcon size={12} className="fill-primary text-primary" />
+            <p className="teext-xs font-semibold">5.0</p>
+          </Badge>
+        </div>
+        {/* Text  */}
+        <div className="px-1 py-3">
+          <h3 className="truncate font-semibold">{barberShop.name}</h3>
+          <p className="truncate text-sm text-gray-400">{barberShop.address}</p>
+          <Button variant="secondary" className="mt-3 w-full">
+            Reservar
+          </Button>
+        </div>
+      </CardContent>
+    </Card>
+  )
+}
+
+export default BarbershopItem
--- app/page.tsx
+++ app/page.tsx
@@ -7,8 +7,12 @@ import Image from "next/image"
 import { Card, CardContent } from "./_components/ui/card"
 import { Badge } from "./_components/ui/badge"
 import { Avatar, AvatarImage } from "./_components/ui/avatar"
+import { db } from "./_lib/prisma"
+import BarbershopItem from "./_components/barbershop-item"
 
-const page = () => {
+const Home = async () => {
+  const barberShops = await db.barberShop.findMany({})
+
   return (
     <div>
       <Header />
@@ -30,16 +34,19 @@ const page = () => {
             src="/banner-01.png"
             alt="Agende com os melhores"
             fill
-            className="rounded-xl object-contain"
+            className="rounded-xl object-cover"
           />
         </div>
         {/* Agendamento */}
-        <Card className="mt-6">
+        <h2 className="mb-3 mt-6 text-xs font-bold uppercase text-gray-400">
+          Agendamentos
+        </h2>
+        <Card>
           <CardContent className="flex justify-between p-0">
             {/* Left */}
             <div className="flex flex-col gap-2 py-5 pl-5">
               <Badge className="w-fit">Confirmado</Badge>
-              <h3 className="font-semibold">Corte de Cabelo</h3>
+              <h3 className="font-bold">Corte de Cabelo</h3>
               <div className="flex items-center gap-2">
                 <Avatar className="h-6 w-6">
                   <AvatarImage src="https://conteudo.imguol.com.br/c/entretenimento/80/2017/04/25/a-atriz-zoe-saldana-como-neytiri-em-avatar-1493136439818_v2_3x4.jpg" />
@@ -55,9 +62,19 @@ const page = () => {
             </div>
           </CardContent>
         </Card>
+
+        {/* Recomendados */}
+        <h2 className="mb-3 mt-6 text-xs font-bold uppercase text-gray-400">
+          Recomendados
+        </h2>
+        <div className="flex gap-4 overflow-auto [&::-webkit-scrollbar]:hidden">
+          {barberShops.map((barberShop) => (
+            <BarbershopItem key={barberShop.id} barberShop={barberShop} />
+          ))}
+        </div>
       </div>
     </div>
   )
 }
 
-export default page
+export default Home
