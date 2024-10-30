d5a81b4742fdaccf2f910fc213474e728b1c8720
flaviogp <flavio.gpinheiro@hotmail.com>
2024-10-30 10:00:33 -0300

    feat: ensure booking details can be viewed

---
+++ app/_components/booking-summary.tsx
@@ -0,0 +1,43 @@
+import React from "react"
+// interface BookingSummaryProps {}
+
+// TODO create a BookingSummary component that displays the booking summary details
+const BookingSummary = () => {
+  return (
+    <></>
+    // <Card>
+    //   <CardContent className="space-y-3 p-3">
+    //     <div className="flex items-center justify-between">
+    //       <h2 className="font-bold">{service.name}</h2>
+    //       <p className="text-sm font-bold">
+    //         {Intl.NumberFormat("pt-BR", {
+    //           style: "currency",
+    //           currency: "BRL",
+    //         }).format(Number(service.price))}
+    //       </p>
+    //     </div>
+    //     {/* Data */}
+    //     <div className="flex items-center justify-between">
+    //       <h2 className="text-sm text-gray-400">Data</h2>
+    //       <p className="text-sm">
+    //         {format(selectedDay, "d 'de' MMMM", {
+    //           locale: ptBR,
+    //         })}
+    //       </p>
+    //     </div>
+    //     {/* Horário */}
+    //     <div className="flex items-center justify-between">
+    //       <h2 className="text-sm text-gray-400">Horário</h2>
+    //       <p className="text-sm">{selectedTime}</p>
+    //     </div>
+    //     {/* Barbearia */}
+    //     <div className="flex items-center justify-between">
+    //       <h2 className="text-sm text-gray-400">Barbearia</h2>
+    //       <p className="text-sm">{barberShop.name}</p>
+    //     </div>
+    //   </CardContent>
+    // </Card>
+  )
+}
+
+export default BookingSummary
